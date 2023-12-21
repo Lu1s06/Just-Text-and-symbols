f52e5ca871692512053ceecbe0168e1dcb5cb555
XeNONE1100 <priinteemps@gmail.com>
2023-12-21 04:22:57 +0000

    added frame limit and other minor improvements :3

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 """game loop and display loop"""
+import random
+import time
+import itertools
+
 import pynput
 from pynput.keyboard import Key
 
-from physics import *
+import physics
+
+
+FPS = 60
+LOOP_DELTA = 1./FPS
 
 HEIGHT = 150
 WIDTH = 200
@@ -14,10 +22,9 @@ def on_press(key):
 
     if key not in currently_pressed:
         currently_pressed.append(key)
-
+        # add keybinds and stuff here :3
         if key == Key.esc:
             pass    # TODO add a pause menu
-        # display_update()
 
 
 def on_release(key):
@@ -28,19 +35,34 @@ def on_release(key):
         pass
     # display_update()
 
-def process_events():
-    with pynput.keyboard.Events() as events:
-        for event in events:
-            if isinstance(event, pynput.keyboard.Events.Release):
-                on_release(event.key)
-            else:
-                on_press(event.key)
-
 
 def main_game_loop():
-    process_events()
+    current_time = target_time = time.perf_counter()
+    for i in itertools.count():
+        #### loop frequency evaluation
+        previous_time, current_time = current_time, time.perf_counter()
+        time_delta = current_time - previous_time
+        print(f'loop #{i} frequency: {1. / time_delta}')
+        #### processing
+        # processing example that sleeps a random time between 0 and LOOP_DELTA/2.
+        time.sleep(random.uniform(0, LOOP_DELTA / 2.))
+
+        #### sleep management
+        target_time += LOOP_DELTA
+        sleep_time = target_time - time.perf_counter()
+        if sleep_time > 0:
+            time.sleep(sleep_time)
+        else:
+            print('took too long')
 
 if __name__ == "__main__":
+
+
+    listener = pynput.keyboard.Listener(
+    on_press=on_press,
+    on_release=on_release)
+
+    listener.start()
 
     while True:
         main_game_loop()
--- physics.py
+++ physics.py
@@ -1,12 +1,21 @@
-
-
 class BaseSprite():
-    
-    def __init__(self, x, y):
 
+    """adds a base sprite class
+    can be referenced in other classes to help implement new sprites"""
+    def __init__(self, x, y, width, height):
+        """currently does nothing"""
         self.x = x
         self.y = y
+        self.width = width
+        self.height = height
 
+    def move(self, dx, dy):
+        """moves the sprite by the specified amount"""
+        self.x += dx
+        self.y += dy
 
-
-
+    def resize(self, dw, dh):
+        """resizes the sprite by the specified amount"""
+        self.width += dw
+        self.height += dh
+        
